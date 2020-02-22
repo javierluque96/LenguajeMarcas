9d934419b9d0b0bf8557dc7a9ec85643eff30894
javierluque96 <javier.lr96@gmail.com>
2020-02-22 23:51:25 +0100

    actualizacion

---
+++ TrabajoBS/javascript.js
@@ -0,0 +1,7 @@
+function comprobarClave(){
+  clave1 = document.f1.clave1.value
+  clave2 = document.f1.clave2.value
+
+  if (clave1 != clave2)
+     alert("Las dos claves son distintas...\nVuelve a rellenar los datos")
+}
