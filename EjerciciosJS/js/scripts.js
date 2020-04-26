53bbbf61b4e616a3490f4de2bb7080179d62a7b2
javierluque96 <javier.lr96@gmail.com>
2020-04-26 23:30:56 +0200

    añadiendo EjercicioJS

---
+++ EjerciciosJS/js/scripts.js
@@ -0,0 +1,15 @@
+$(window).scroll(function() {
+  if ($(document).scrollTop() > 600) {
+    $('.nav').addClass('affix');
+    console.log("OK");
+  } else {
+    $('.nav').removeClass('affix');
+  }
+});
+
+$('.hamburguesa').click(function() {
+  $(this).toggleClass('active');
+  console.log("Clicked menu");
+  $("#mainListDiv").toggleClass("show_list");
+  $("#mainListDiv").fadeIn();
+});
