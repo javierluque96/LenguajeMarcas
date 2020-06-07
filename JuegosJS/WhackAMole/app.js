1214c6fcd27c670b20909b970848685f14fcec1e
javierluque96 <javier.lr96@gmail.com>
2020-06-07 13:57:17 +0200

    Whack a mole

---
+++ JuegosJS/WhackAMole/app.js
@@ -0,0 +1,46 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#seconds')
+let score = document.querySelector('#score')
+
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+  square.forEach(className => {
+    className.classList.remove('mole')
+  })
+  let randomPosition = square[Math.floor(Math.random() * 9)]
+  randomPosition.classList.add('mole')
+
+  // Assign the id of the randomPosition for us to use later
+  hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+  id.addEventListener('mouseup', () => {
+    if (id.id === hitPosition) {
+      result++
+      score.textContent = result
+    }
+  })
+})
+
+function moveMole() {
+  let timerId = null
+  timerId = setInterval(randomSquare, 600)
+}
+
+moveMole()
+
+function countDown() {
+  currentTime--
+  timeLeft.textContent = currentTime
+
+  if (currentTime === 0) {
+    clearInterval(timerId)
+    alert('GAME OVER! Your score is ' + result)
+  }
+}
+
+let timerId = setInterval(countDown, 1000)
